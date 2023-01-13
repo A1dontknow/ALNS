4fd4de6614fc86152646d158cd479f3c8926b6c2
unknown <trannamkhanh_t65@hus.edu.vn>
2023-01-13 14:04:13 +0700

    Empty

--- CVRP/Config.py
+++ CVRP/Config.py
@@ -7,13 +7,16 @@ class Config:
 
         * Nhung tham so nen giu co dinh:
         - D: Deterministic level cho Shaw Destroy. Khong co khac biet nhieu neu thay doi
-        - ITERATION: So vong lap thuc hien thuat toan. Khi cho chay du lau, thuat toan se
-        co the tim duoc nhung nghiem tot hon, nhung khong co nghia la cang lau cang tot.
-        Chay thuat toan song song voi so lan lap nao do co the la mot y tuong tot
         - DESTROY_RATIO: Muc pha huy nghiem trong destroy operator (tru MassiveDestroy).
         Muc Destroy nen de la 10%
 
         * Nhung tham so co the tinh chinh:
+        - ITERATION: So vong lap thuc hien thuat toan. Khi cho chay du lau, thuat toan se
+        co the tim duoc nhung nghiem tot hon, nhung khong co nghia la cang lau cang tot.
+        Chay thuat toan song song voi so lan lap nao do co the la mot y tuong tot
+        - SEGMENT: So phan doan cua thuat toan. Phan doan la cac doan chia Iteration.
+        Tham so nay se quyet dinh so lan update trong so Destroy/Repair va so lan dung 
+        Local Search
         - PHI_1: Diem thuong cho operator neu tim duoc nghiem tot nhat
         - PHI_2: Diem thuong cho operator neu tim duoc nghiem tot hon nghiem hien tai
         - PHI_3: Diem thuong neu nghiem tim duoc khong co su cai tien nao
@@ -24,13 +27,17 @@ class Config:
         - BIG_DEVIATION: Chap nhan nghiem (ke ca nghiem xau hon) neu khong te hon mot nguong
         so voi nghiem tot nhat sau khi trai qua rat nhieu lan lap ma khong co su cai tien ve
         nghiem
+        
+        Reference: Ropke, S., & Pisinger, D. (2006) - An Adaptive Large Neighborhood Search 
+        Heuristic for the Pickup and Delivery Problem with Time Windows. Transportation Science,
+	40(4), 455–472. doi:10.1287/trsc.1050.0135
 
     """
     PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
     D = 5
-    ITERATION = 100000
+    ITERATION = 30000
     DESTROY_RATIO = 0.1
-    SEGMENT = 1000
+    SEGMENT = 300
     PHI_1 = 10
     PHI_2 = 6
     PHI_3 = 1
